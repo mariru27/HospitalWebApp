e02d01d80baeb5becf43556c32350c9879495c25
mariru27 <marina.rusu.99@gmail.com>
2020-12-26 00:30:45 +0200

    add header and footer

---
+++ app/footer.php
@@ -0,0 +1,9 @@
+
+ <footer class="border-top footer text-muted">
+<h1>footer<h1>
+ </footer>
+ <!-- <script src="~/lib/jquery/dist/jquery.min.js"></script>
+ <script src="~/lib/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+ <script src="~/js/site.js" asp-append-version="true"></script> -->
+</body>
+</html>
---
+++ app/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <h1>headerr<h1>
+    <!-- // <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    // <link rel="stylesheet" href="~/lib/bootstrap/dist/css/bootstrap.min.css" />
+    // <link rel="stylesheet" href="~/css/site.css" /> -->
+</head>
+<body>
+    <header>
+
+    </header>
