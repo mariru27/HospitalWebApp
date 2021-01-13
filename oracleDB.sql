dfb89361a0fd0e77b29c0835d4ab7384c20449e0
mariru27 <marina.rusu.99@gmail.com>
2021-01-13 22:55:46 +0200

    PACIENT TABLE

--- oracleDB.sql
+++ oracleDB.sql
@@ -46,43 +46,12 @@ CREATE TABLE MEDIC(
     CONSTRAINT fk_idReteta FOREIGN KEY (idReteta) REFERENCES RETETA(idReteta)
 );
 
-
---need to create tables
-
-CREATE TABLE TRATAMENT(
-    idTratament NUMBER(6) PRIMARY KEY,
-    descriere VARCHAR(500),
-    CONSTRAINT FK_Reteta FOREIGN KEY (idDiagnostic),
-    REFERENCES DIAGNOSTIC(idDiagnostic)
-);
-
-CREATE TABLE MEDIC(
-    idMedic NUMBER(6) PRIMARY KEY,
-    nume VARCHAR(255),
-    prenume VARCHAR(255),
-    CONSTRAINT FK_Reteta FOREIGN KEY (idReteta),
-    REFERENCES RETETA(idReteta)
-);
-
 CREATE TABLE PACIENT(
-    cnp NUMBER(13) PRIMARY KEY,
+    cnp NUMBER(13),
     nume VARCHAR(255),
     prenume VARCHAR(255),
     tip_asigurare VARCHAR(255),
-    CONSTRAINT FK_Reteta FOREIGN KEY (idReteta),
-    REFERENCES RETETA(idReteta)
+    idRetetaPacient NUMBER(6),
+    CONSTRAINT pk_cnp PRIMARY KEY(cnp),
+    CONSTRAINT fk_idRetetaPacient FOREIGN KEY(idRetetaPacient) REFERENCES RETETA(idReteta)
 );
-
-CREATE TABLE RETETA(
-    idReteta NUMBER(6),
-    codfiscal VARCHAR(255),
-	unitatemedicala VARCHAR(255),
-	judet VARCHAR(255),
-	nr_casa_asig_medic NUMBER(10),
-    CONSTRAINT FK_Medicament FOREIGN KEY (idMedicament),
-    REFERENCES MEDICAMENT(idMedicament)
-    CONSTRAINT FK_Tratament FOREIGN KEY (idTratament),
-    REFERENCES TRATAMENT(idTratament)
-);
-
-
