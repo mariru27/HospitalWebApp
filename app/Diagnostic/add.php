bf61b04e9788ad10a78427e7c6cbc7eedd9333dc
mariru27 <marina.rusu.99@gmail.com>
2021-01-16 18:02:38 +0200

    display Diagnostic input fields

--- app/Diagnostic/add.php
+++ app/Diagnostic/add.php
@@ -4,3 +4,23 @@
 ?>
 
 <h4 class="display-4">Adauga Diagnostic</h4>
+
+
+<div class="row">
+    <div class="col-md-4">
+        <form action="add.php" method="get">
+        <input type="hidden" name="action" value="add">
+            <div class="form-group">
+                <label for = "denumire" class="control-label">Denumire</label>
+                <input class="form-control" type="text" id="denumire" name = "denumire" rows="3"> 
+            </div>
+            <div class="form-group">
+                <label for = "tip" class="control-label">Tip</label>
+                <input class="form-control" type="text" id="tip" name = "tip" rows="3">
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Adauga" class="btn btn-primary" />
+            </div>
+        </form>
+    </div>
+</div>
