92cab47e530ccdcec5fa1b4492ab7183c610f668
mariru27 <marina.rusu.99@gmail.com>
2021-01-17 19:59:47 +0200

    delete Reteta implemented

--- app/Reteta/Index.php
+++ app/Reteta/Index.php
@@ -1,10 +1,19 @@
 <?php
   include '../header.php';
   include_once "functions.php";
+  include_once "test.php";
+
 ?>
 
 <h4 class="display-4">Reteta</h4>
 
 <?php
+  $action = isset($_REQUEST['action'])? $_REQUEST['action']:"";
+  if($action == 'delete')
+  {
+    //call delete function
+    $idReteta = $_REQUEST['idReteta'];
+    DeleteReteta1($idReteta);
+  }
   AfiseazaRetete();
 ?>
--- app/Reteta/test.php
+++ app/Reteta/test.php
@@ -1,1 +1,12 @@
-<h1>is a test<h1>
+<?php
+
+function DeleteReteta1($idReteta)
+{
+  global $conn;
+
+  $queryDelete = sprintf("DELETE FROM RETETA WHERE IDRETETA=%d", $idReteta);
+  $resultDelete = oci_parse($conn, $queryDelete);
+  oci_execute($resultDelete);
+
+}
+?>
