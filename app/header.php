ad8e443cab4b7362ff7fe43ccc5fda931929c684
mariru27 <marina.rusu.99@gmail.com>
2021-01-19 00:37:17 +0200

    I do not need more Pacient-Reteta, Medic-Reteta, Medicament-Reteta

--- app/header.php
+++ app/header.php
@@ -13,45 +13,44 @@
 <body>
 <header>
 
-  <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
-              <div class="container">
-                  <a href="<?php  $_SERVER["DOCUMENT_ROOT"];?>/HospitalWebApp/app/Home.php" class="navbar-brand">Hospital Web App</a>
-                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="navbarSupportedContent"
-                          aria-expanded="false" aria-label="Toggle navigation">
-                      <span class="navbar-toggler-icon"></span>
-                  </button>
-                  <div class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse">
-                      <ul class="navbar-nav flex-grow-1">
-
-                              <li class="nav-item">
-                                  <a class="nav-link text-dark" href="<?php  $_SERVER["DOCUMENT_ROOT"];?>/HospitalWebApp/app/Reteta/index.php">Reteta</a>
-                              </li>
-                              <li class="nav-item">
-                                  <a class="nav-link text-dark" href="<?php  $_SERVER["DOCUMENT_ROOT"];?>/HospitalWebApp/app/Pacient/index.php">Pacient</a>
-                              </li>
-                              <li class="nav-item">
-                                  <a class="nav-link text-dark" href="<?php  $_SERVER["DOCUMENT_ROOT"];?>/HospitalWebApp/app/Medicament/index.php">Medicament</a>
-                              </li>
-                              <li class="nav-item">
-                                  <a class="nav-link text-dark" href="<?php  $_SERVER["DOCUMENT_ROOT"];?>/HospitalWebApp/app/Diagnostic/index.php">Diagnostic</a>
-                              </li>
-                              <li class="nav-item">
-                                  <a class="nav-link text-dark" href="<?php  $_SERVER["DOCUMENT_ROOT"];?>/HospitalWebApp/app/Medic/index.php">Medic</a>
-                              </li>
-                              <li class="nav-item">
-                                  <a class="nav-link text-dark" href="<?php  $_SERVER["DOCUMENT_ROOT"];?>/HospitalWebApp/app/Tratament/index.php">Tratament</a>
-                              </li>
-                              <li class="nav-item">
-                                  <a class="nav-link text-dark" href="<?php  $_SERVER["DOCUMENT_ROOT"];?>/HospitalWebApp/app/Medic_Reteta/index.php">Medic Reteta</a>
-                              </li>
-                              <li class="nav-item">
-                                  <a class="nav-link text-dark" href="<?php  $_SERVER["DOCUMENT_ROOT"];?>/HospitalWebApp/app/Pacient_Reteta/index.php">Pacient Reteta</a>
-                              </li>
-                      </ul>
-                  </div>
-              </div>
-          </nav>
-          
-
-
+<nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
+            <div class="container">
+                <a href="<?php  $_SERVER["DOCUMENT_ROOT"];?>/HospitalWebApp/app/Home.php" class="navbar-brand">Hospital Web App</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse">
+                    <ul class="navbar-nav flex-grow-1">
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href="<?php  $_SERVER["DOCUMENT_ROOT"];?>/HospitalWebApp/app/Reteta/index.php">Reteta</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href="<?php  $_SERVER["DOCUMENT_ROOT"];?>/HospitalWebApp/app/Pacient/index.php">Pacient</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href="<?php  $_SERVER["DOCUMENT_ROOT"];?>/HospitalWebApp/app/Medicament/index.php">Medicament</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href="<?php  $_SERVER["DOCUMENT_ROOT"];?>/HospitalWebApp/app/Diagnostic/index.php">Diagnostic</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href="<?php  $_SERVER["DOCUMENT_ROOT"];?>/HospitalWebApp/app/Medic/index.php">Medic</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href="<?php  $_SERVER["DOCUMENT_ROOT"];?>/HospitalWebApp/app/Tratament/index.php">Tratament</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href="<?php  $_SERVER["DOCUMENT_ROOT"];?>/HospitalWebApp/app/Medic_Reteta/index.php">Medic Reteta</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href="<?php  $_SERVER["DOCUMENT_ROOT"];?>/HospitalWebApp/app/Pacient_Reteta/index.php">Pacient Reteta</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href="<?php  $_SERVER["DOCUMENT_ROOT"];?>/HospitalWebApp/app/Medicament_Reteta/index.php">Pacient Reteta</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 </header>
