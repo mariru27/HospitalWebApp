9aec6b05f2671df786a5271f8aeb95108f7b9755
mariru27 <marina.rusu.99@gmail.com>
2021-01-21 13:24:27 +0200

    validate edit medicament

--- app/Medicament/edit.php
+++ app/Medicament/edit.php
@@ -7,30 +7,53 @@
 
 
 <?php 
-  //medicament to display
-  global $conn;
+    //medicament to display
+    global $conn;
 
-  $idMedicament = $_REQUEST['idMedicament'];
-  $querySelect = sprintf("SELECT * FROM MEDICAMENT WHERE IDMEDICAMENT=%d", $idMedicament);
-  $resultSelect = oci_parse($conn, $querySelect);
-  oci_execute($resultSelect);
-  $rowPacient = oci_fetch_array($resultSelect, OCI_ASSOC+OCI_RETURN_NULLS);
+    $idMedicament = $_REQUEST['idMedicament'];
+    $querySelect = sprintf("SELECT * FROM MEDICAMENT WHERE IDMEDICAMENT=%d", $idMedicament);
+    $resultSelect = oci_parse($conn, $querySelect);
+    oci_execute($resultSelect);
+    $rowPacient = oci_fetch_array($resultSelect, OCI_ASSOC+OCI_RETURN_NULLS);
 
-   //edit action
-   $action = isset($_REQUEST['action'])? $_REQUEST['action']:"";
-   if($action == 'edit')
-   {
-    $denumire = $_REQUEST['denumire'];
-    $cantitate = $_REQUEST['cantitate'];
- 
-     //update in database
-     $queryUpdate = sprintf("UPDATE MEDICAMENT SET DENUMIRE = '%s', CANTITATE = %d WHERE IDMEDICAMENT = %d",
-                             $denumire, $cantitate, $idMedicament);
-     $resultUpdate = oci_parse($conn, $queryUpdate);
-     oci_execute($resultUpdate); 
-     header("Location: http://localhost/HospitalWebApp/app/Medicament/index.php");
- 
-   }
+    //for validation
+    $valid = true;
+    $errorDenumire = "";
+    $errorCantitate = "";
+
+    //edit action
+    $action = isset($_REQUEST['action'])? $_REQUEST['action']:"";
+    if($action == 'edit')
+    {
+        $denumire = $_REQUEST['denumire'];
+        $cantitate = $_REQUEST['cantitate'];
+        if($denumire == null)
+        {
+            $errorDenumire = "Campul denumire trebuie completat";
+            $valid = false;
+        }
+        if($cantitate == null)
+        {
+            $errorCantitate = "Campul tip trebuie completat";
+            $valid = false;
+        }
+        if(!is_numeric($cantitate) && $cantitate != null)
+        {
+            $errorCantitate = "Campul trebuie sa contina doar numere";
+            $valid = false;
+        }
+
+        if($valid == true)
+        {
+            //update in database
+            $queryUpdate = sprintf("UPDATE MEDICAMENT SET DENUMIRE = '%s', CANTITATE = %d WHERE IDMEDICAMENT = %d",
+                                    $denumire, $cantitate, $idMedicament);
+            $resultUpdate = oci_parse($conn, $queryUpdate);
+            oci_execute($resultUpdate); 
+            header("Location: http://localhost/HospitalWebApp/app/Medicament/index.php");
+        }
+
+    }
 ?>
 
 <div class="row">
@@ -41,10 +64,12 @@
             <div class="form-group">
                 <label for = "denumire" class="control-label">Denumire</label>
                 <input class="form-control" type="text" id="denumire" name = "denumire" value ="<?php echo $rowPacient['DENUMIRE']?>"> 
+                <span style="color:red;" class="help-block"><?php echo $errorDenumire; ?></span>
             </div>
             <div class="form-group">
                 <label for = "cantitate" class="control-label">Cantitate</label>
                 <input class="form-control" type="text" id="cantitate" name = "cantitate" value ="<?php echo $rowPacient['CANTITATE']?>">
+                <span style="color:red;" class="help-block"><?php echo $errorCantitate; ?></span>
             </div>
             <div class="form-group">
                 <input type="submit" value="Editeaza" class="btn btn-primary" />
