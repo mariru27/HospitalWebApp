4a58dc552e3155ac5673008a14d5ca37a97f8df6
mariru27 <marina.rusu.99@gmail.com>
2021-01-21 16:00:13 +0200

    validation edit pacient

--- app/Pacient/edit.php
+++ app/Pacient/edit.php
@@ -7,14 +7,22 @@
 
 
 <?php 
-  //pacient to display
-  global $conn;
+    //pacient to display
+    global $conn;
 
-  $idPacient = $_REQUEST['idPacient'];
-  $querySelect = sprintf("SELECT * FROM PACIENT WHERE IDPACIENT=%d", $idPacient);
-  $resultSelect = oci_parse($conn, $querySelect);
-  oci_execute($resultSelect);
-  $rowPacient = oci_fetch_array($resultSelect, OCI_ASSOC+OCI_RETURN_NULLS);
+    $idPacient = $_REQUEST['idPacient'];
+    $querySelect = sprintf("SELECT * FROM PACIENT WHERE IDPACIENT=%d", $idPacient);
+    $resultSelect = oci_parse($conn, $querySelect);
+    oci_execute($resultSelect);
+    $rowPacient = oci_fetch_array($resultSelect, OCI_ASSOC+OCI_RETURN_NULLS);
+
+
+    //for validation
+    $valid = true;
+    $errorCnp = "";
+    $errorNume = "";
+    $errorPrenume = "";
+    $errorTipAsigurare = "";
 
    //edit action
    $action = isset($_REQUEST['action'])? $_REQUEST['action']:"";
@@ -26,14 +34,58 @@
      $prenume = $_REQUEST['prenume'];
      $tipAsigurare = $_REQUEST['tipAsigurare'];
 
-     echo $cnp . " " . $nume . " " . $prenume . " " . $tipAsigurare;
- 
-     //update in database
-     $queryUpdate = sprintf("UPDATE PACIENT SET CNP = %d, NUME = '%s', PRENUME = '%s', TIP_ASIGURARE = '%s' WHERE IDPACIENT = '%d'",
-                             $cnp, $nume, $prenume, $tipAsigurare, $idPacient);
-     $resultUpdate = oci_parse($conn, $queryUpdate);
-     oci_execute($resultUpdate); 
-     header("Location: http://localhost/HospitalWebApp/app/Pacient/index.php");
+     if($cnp == null)
+     {
+         $errorCnp = "Campul cnp trebuie completat";
+         $valid = false;
+     }
+     if(!is_numeric($cnp) && $cnp != null)
+     {
+         $errorCnp = "Campul cnp trebuie sa contina cifre";
+         $valid = false;
+     }
+
+     if(is_numeric($cnp) && strlen($cnp) != 13)
+     {
+         $errorCnp = "Campul cnp trebuie sa contina 13 cifre";
+         $valid = false;
+
+
+     }
+     $querySelect = sprintf("SELECT * FROM PACIENT WHERE pacient.cnp = %d", $cnp);
+     $resultSelect = oci_parse($conn, $querySelect);
+     oci_execute($resultSelect);   
+
+     if(is_numeric($cnp) && strlen($cnp) == 13 && (oci_fetch_row ($resultSelect) != null && $rowPacient['CNP'] != $cnp))
+     {
+         $errorCnp = "Acest cnp exista deja in baza de date";
+         $valid = false;
+     }
+
+     if($nume == null)
+     {
+         $errorNume = "Campul nume trebuie completat";
+         $valid = false;
+     }
+     if($prenume == null)
+     {
+         $errorPrenume = "Campul prenume trebuie completat";
+         $valid = false;
+     }
+     if($tipAsigurare == null)
+     {
+         $errorTipAsigurare = "Campul tip asigurare trebuie completat";
+         $valid = false;
+     }
+     if($valid == true)
+     {
+        //update in database
+        $queryUpdate = sprintf("UPDATE PACIENT SET CNP = %d, NUME = '%s', PRENUME = '%s', TIP_ASIGURARE = '%s' WHERE IDPACIENT = '%d'",
+                                $cnp, $nume, $prenume, $tipAsigurare, $idPacient);
+        $resultUpdate = oci_parse($conn, $queryUpdate);
+        oci_execute($resultUpdate); 
+        header("Location: http://localhost/HospitalWebApp/app/Pacient/index.php");
+     }
  
    }
 ?>
@@ -47,18 +99,22 @@
         <div class="form-group">
                 <label for = "cnp" class="control-label">Cnp</label>
                 <input class="form-control" type="text" id="cnp" name = "cnp" value ="<?php echo $rowPacient['CNP']?>"> 
+                <span style="color:red;" class="help-block"><?php echo $errorCnp; ?></span>
             </div>
             <div class="form-group">
                 <label for = "nume" class="control-label">Nume</label>
                 <input class="form-control" type="text" id="nume" name = "nume" value ="<?php echo $rowPacient['NUME']?>" >
+                <span style="color:red;" class="help-block"><?php echo $errorNume; ?></span>
             </div>
             <div class="form-group">
                 <label for = "prenume" class="control-label">Prenume</label>
                 <input class="form-control" type="text" id="prenume" name = "prenume" value ="<?php echo $rowPacient['PRENUME']?>">
+                <span style="color:red;" class="help-block"><?php echo $errorPrenume; ?></span>
             </div>
             <div class="form-group">
                 <label for = "tipAsigurare" class="control-label">Tip asigurare</label>
                 <input class="form-control" type="text" id="tipAsigurare" name = "tipAsigurare" value ="<?php echo $rowPacient['TIP_ASIGURARE']?>">
+                <span style="color:red;" class="help-block"><?php echo $errorTipAsigurare; ?></span>
             </div>
             <div class="form-group">
                 <input type="submit" value="Editeaza" class="btn btn-primary" />
