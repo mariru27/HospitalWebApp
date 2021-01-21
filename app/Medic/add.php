eb61de695c5519de042d642c7080a25c3e37590d
mariru27 <marina.rusu.99@gmail.com>
2021-01-21 13:09:31 +0200

    validate medic, add

--- app/Medic/add.php
+++ app/Medic/add.php
@@ -12,15 +12,32 @@
     global $conn;
 
     $action = isset($_REQUEST['action'])? $_REQUEST['action']:"";
+    $valid = true;
+    $errorNume = "";
+    $errorPrenume = "";
+
     if($action == 'add')
     {
         $nume = $_REQUEST['nume'];
         $prenume = $_REQUEST['prenume'];
+        if($nume == null)
+        {
+            $errorNume = "Campul nume trebuie completat";
+            $valid = false;
+        }
+        if($prenume == null)
+        {
+            $errorPrenume = "Campul prenume trebuie completat";
+            $valid = false;
+        }
+        if($valid == true)
+        {
 
-        $queryInsert = sprintf("INSERT INTO MEDIC VALUES(seq_medic.nextval,'%s', '%s')", $nume, $prenume);
-        $resultInsert = oci_parse($conn, $queryInsert);
-        oci_execute($resultInsert);
-        header("Location: http://localhost/HospitalWebApp/app/Medic/index.php");
+            $queryInsert = sprintf("INSERT INTO MEDIC VALUES(seq_medic.nextval,'%s', '%s')", $nume, $prenume);
+            $resultInsert = oci_parse($conn, $queryInsert);
+            oci_execute($resultInsert);
+            header("Location: http://localhost/HospitalWebApp/app/Medic/index.php");
+        }
     }
 ?>
 
@@ -31,10 +48,12 @@
             <div class="form-group">
                 <label for = "nume" class="control-label">Nume</label>
                 <input class="form-control" type="text" id="nume" name = "nume" rows="3"> 
+                <span style="color:red;" class="help-block"><?php echo $errorNume; ?></span>
             </div>
             <div class="form-group">
                 <label for = "prenume" class="control-label">Prenume</label>
                 <input class="form-control" type="text" id="prenume" name = "prenume" rows="3">
+                <span style="color:red;" class="help-block"><?php echo $errorPrenume; ?></span>
             </div>
             <div class="form-group">
                 <input type="submit" value="Adauga" class="btn btn-primary" />
